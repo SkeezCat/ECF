8d0762ba3b67e64d23850c980202d03cf77cb35b
Gastounz <raphael-braillard@outlook.fr>
2023-07-24 16:59:58 +0200

    Récupération du résultat de la recherche dans movies.php

--- controller/movies.php
+++ controller/movies.php
@@ -4,10 +4,18 @@
 $moviesDAO = new MoviesDAO();
 $movies = $moviesDAO->getAll();
 
-// var_dump($movies->get)
+$search;
+
+// Récupère le résultat de la recherche depuis l'URL
+$queryResult = parse_url($_SERVER['REQUEST_URI'], PHP_URL_QUERY);
+
+if ($queryResult != null) {
+    $search = explode($queryResult)[1];
+}
 
 // Affichage du template Movies
 echo $twig->render('movies.html.twig', [
-    'movies' => $movies
+    'movies' => $movies,
+    'search' => $search
 ]);
 ?>
