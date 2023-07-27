e693b1d5d3125fce58bd4bb541a209790eac67c7
LuSkeez <benedettolucie@gmail.com>
2023-07-27 09:42:59 +0200

    classe user terminée

--- model/BO/User.php
+++ model/BO/User.php
@@ -2,11 +2,34 @@
 class User {
     private $email, $username, $password;
 
-    function __construct(string $email, string $username, string $password) {
+    public function __construct(string $email, string $username, string $password) {
         $this->setEmail($email);
         $this->setUsername($username);
         $this->setPassword($password);
     }
+
+    public function getEmail() {
+        return $this ->email;
+    }
+
+    public function setEmail($email){
+        $this->email = $email;
+    } 
+    public function getUsername() {
+        return $this ->username;
+    }
+
+    public function setUsername($username){
+        $this->username = $username;
+    } 
+
+    public function getPassword() {
+        return $this ->password;
+    }
+
+    public function setPassword($password){
+        $this->password = $password;
+    } 
 
 }
 ?>
