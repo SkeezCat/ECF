3e59becb5e5deb75492091fd2dd59e4f7e82573a
Gastounz <raphael-braillard@outlook.fr>
2023-07-27 11:39:24 +0200

    condition user dans la navbar

--- model/DAL/UsersDAO.php
+++ model/DAL/UsersDAO.php
@@ -1,3 +1,6 @@
-//elseif ($query = $this->BDD->prepare('SELECT * FROM Users WHERE username= :username')){
-            $query->execute(array(':username' => $user->getUsername()));
-            echo "Ce pseudo est déjà utilisé.";
+<?php
+class UsersDAO extends Dao {
+
+
+}
+?>
