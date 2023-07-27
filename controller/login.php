290b4067e17317287f6b4e8e1eb9dcf60c0937a5
LuSkeez <benedettolucie@gmail.com>
2023-07-27 11:11:31 +0200

    tout fini ?

--- controller/login.php
+++ controller/login.php
@@ -1,4 +1,16 @@
 <?php 
+// Initialisation des variables
+$usersDao = new UsersDAO();
+$user = "";
+
+// Vérification des identifiants
+if (isset($_POST['email'])) {
+    $user = $usersDao->verifOne(new User($_POST['email'], $_POST['username'], $_POST['password']));
+    
+    if ($user != false) {
+        $_SESSION['user'] = $user->getEmail();
+    }
+}
 
 // Affichage du template Login
 echo $twig->render('login.html.twig');
