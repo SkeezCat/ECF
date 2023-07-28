20165c52077a26e7c8a9e40b6ef97119a84416c8
Gastounz <raphael-braillard@outlook.fr>
2023-07-28 11:20:28 +0200

    fini !

--- controller/create_account.php
+++ controller/create_account.php
@@ -4,30 +4,27 @@ $user = null;
 
 
 // Affichage du template Create_Account
-if (isset($_POST["email"]) && isset($_POST["password"])){
+if (isset($_POST["email"]) && isset($_POST["password"])) {
     $usersDAO = new UsersDAO();
-if (empty($_POST["username"])) {
-            $message = "Le champs Username est vide";}
-if (!preg_match('#^[a-zA-Z0-9]+$#', $_POST["username"])) {
-            $message = "Le Username doit être renseigné en lettres minuscules sans accents, sans caractères spéciaux.";} 
-        
-if (empty($_POST["email"])){
-        $message = "Le champs Email est vide.";
 
-} if (!preg_match('/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/i', $_POST["email"])) {
+    if (!preg_match('#^[a-zA-Z0-9]+$#', $_POST["username"])) {
+        $message = "Le Username doit être renseigné en lettres minuscules sans accents, sans caractères spéciaux.";
+
+    } else if (!preg_match('/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/i', $_POST["email"])) {
         $message = "L'E-mail n'est pas au bon format.";
-
-} if(empty($_POST["password"])){
-            $message = "Le champs Password est vide.";
         
-} if(($_POST["password"])!==($_POST["password2"])){
+    } else if (($_POST["password"])!==($_POST["password2"])) {
         $message = "Les deux Passwords doivent être identiques.";
     
-} else {
-    $message = "Votre compte a bien été créé.";
-    $user = $usersDAO ->add(new User($_POST["email"], $_POST["password"], $_POST["username"]));
-    header('Location: login');
-}
+    } else {
+        try {
+            $message = "Votre compte a bien été créé.";
+            $user = $usersDAO ->add(new User($_POST["email"], $_POST["password"], $_POST["username"]));
+            header('Location: login');
+        } catch (PDOException $e) {
+            $message = $e->getMessage();
+        }
+    }
 }
 
 echo $twig->render('create_account.html.twig', [
--- model/DAL/UsersDAO.php
+++ model/DAL/UsersDAO.php
@@ -19,62 +19,59 @@ class UsersDAO extends Dao {
     public function verifOne($user)
     {
         $query = $this->BDD->prepare('SELECT * FROM users WHERE email = :email/* AND password = :password*/');
-    $query->execute(array(':email' => $user->getEmail()/*, ':password' => $this->verifPassword($user->getPassword(), 'password')*/));
-    $data = $query->fetch();
-    if (!$data || !$this->verifPassword($user->getPassword(), $data['password'])) {
-        return false;
+        $query->execute(array(':email' => $user->getEmail()/*, ':password' => $this->verifPassword($user->getPassword(), 'password')*/));
+        $data = $query->fetch();
 
-    } else {
-        $user = new User($data['email'], $data['password'], $data['username']);
+        if (!$data || !password_verify($user->getPassword(), $data['password'])) {
+            return false;
+        } else {
+            $user = new User($data['email'], $data['password'], $data['username']);
+            return $user;
+        }
+    }
+
+    // Récupère un utilisateur
+    public function getOne($email)
+    {
+        $query = $this->BDD->prepare('SELECT * FROM users WHERE email = :email');
+        $query->execute(array(':email' => $email));
+        $data = $query->fetch();
+        $user = new User($data['email'], $data['username'], $data['password']);
         return $user;
     }
-}
 
-// Vérifie la correspondance du mot de passe saisi avec la base de données
-private function verifPassword($password, $hashedPassword) {
-    if (password_verify($password, $hashedPassword)) {
-        return $password;
-    } else {
-        return false;
+    // Ajouter un utilisateur
+    public function add($data)
+    {
+        $query = $this->BDD->prepare('INSERT INTO users (email, password, username) VALUES (:email, :password, :username)');
+        $values = [
+            'email' => $data->getEmail(), 
+            'username' => $data->getUsername(), 
+            'password' => password_hash($data->getPassword(), PASSWORD_DEFAULT)
+        ];
+
+        return $query->execute($values);
     }
-}
 
-// Récupère un utilisateur
-public function getOne($email)
-{
-    $query = $this->BDD->prepare('SELECT * FROM users WHERE email = :email');
-    $query->execute(array(':email' => $email));
-    $data = $query->fetch();
-    $user = new User($data['email'], $data['username'], $data['password']);
-    return $user;
-}
-
-// Ajouter un utilisateur
-public function add($data)
-{
-    $values = ['email' => $data->getEmail(), 'username' => $data->getUsername(), 'password' => password_hash($data->getPassword(), PASSWORD_DEFAULT)];
-    $query = $this->BDD->prepare('INSERT INTO users (email, password, username) VALUES (:email, :password, :username)');
-    return $query->execute($values);
-}
-
-//Modifier un utilisateur
-public function update($user) 
-{
-    $valeurs = ['email' => $user->getEmail(), 'username' => $user->getUsername(), 'password' => $user->getPassword()];
-    $query = 'UPDATE users SET email = :email, password = :password WHERE id = :id';
-    $update = $this->BDD->prepare($query);
-    if (!$update->execute($valeurs)) {
-        return false;
-    } else {
-        return true;
+    //Modifier un utilisateur
+    public function update($user) 
+    {
+        $valeurs = ['email' => $user->getEmail(), 'username' => $user->getUsername(), 'password' => $user->getPassword()];
+        $query = 'UPDATE users SET email = :email, password = :password WHERE id = :id';
+        $update = $this->BDD->prepare($query);
+        
+        if (!$update->execute($valeurs)) {
+            return false;
+        } else {
+            return true;
+        }
     }
-}
 
-// Supprime un utilisateur
-public function delete($email)
-{
-    $query = $this->BDD->prepare('DELETE FROM users WHERE users.email = :email_user');
-    $query->execute(array(':email_user' => $email));
-}
+    // Supprime un utilisateur
+    public function delete($email)
+    {
+        $query = $this->BDD->prepare('DELETE FROM users WHERE users.email = :email_user');
+        $query->execute(array(':email_user' => $email));
+    }
 }
 ?>
